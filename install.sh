58998ad986a776230372aeed0444f7902b271c96
Ari Birnbaum <ari@ceiphr.com>
2023-06-24 12:49:33 -0400

    Added error handling to install script. Fixed oh-my-zsh install issue. Minor refactoring.

--- install.sh
+++ install.sh
@@ -15,15 +15,7 @@ if [[ -n "${NO_COLOR}" ]]; then
     TXT_RED='\033[0m'
 fi
 
-PLUGINS=(
-    "zsh-autosuggestions"
-    "zsh-completions"
-    "zsh-interactive-cd"
-    "zsh-syntax-highlighting"
-)
-THEMES=("powerlevel10k")
-
-git pull origin main
+git pull origin main >/dev/null 2>&1 || error "Unable to pull latest changes. Try again."
 
 # Tiago Lopo: https://stackoverflow.com/a/29436423/9264137
 # This is used to ask the user for confirmation.
@@ -50,31 +42,26 @@ function error() {
 }
 
 function dotfiles_reset() {
-    unset -f yes_or_no install_plugins install dotfiles_reset
+    unset -f yes_or_no error install_omzsh install dotfiles_reset
 }
 
-function install_plugins() {
+function install_omzsh() {
     CUSTOM_DIR="${ZSH_CUSTOM:-$ZSH/custom}"
 
-    # Remove old plugins
-    for plugin in "${PLUGINS[@]}"; do
-        rm -rf "${CUSTOM_DIR}"/plugins/"${plugin}"
-    done
-    for theme in "${THEMES[@]}"; do
-        rm -rf "${CUSTOM_DIR}"/themes/"${theme}"
-    done
+    # Remove old oh-my-zsh installation
+    rm -rf "$ZSH"
 
     # Install oh-my-zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended >/dev/null 2>&1 || error "Unable to install oh-my-zsh. Try again."
 
     # Install zsh theme
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k "${CUSTOM_DIR}"/themes/powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k "${CUSTOM_DIR}"/themes/powerlevel10k >/dev/null 2>&1 || error "Unable to install powerlevel10k. Try again."
 
     # Install zsh plugins
-    git clone https://github.com/zsh-users/zsh-autosuggestions "${CUSTOM_DIR}"/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-completions "${CUSTOM_DIR}"/plugins/zsh-completions
-    git clone https://github.com/changyuheng/zsh-interactive-cd "${CUSTOM_DIR}"/plugins/zsh-interactive-cd
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting "${CUSTOM_DIR}"/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions "${CUSTOM_DIR}"/plugins/zsh-autosuggestions >/dev/null 2>&1 || error "Unable to install zsh-autosuggestions. Try again."
+    git clone https://github.com/zsh-users/zsh-completions "${CUSTOM_DIR}"/plugins/zsh-completions >/dev/null 2>&1 || error "Unable to install zsh-completions. Try again."
+    git clone https://github.com/changyuheng/zsh-interactive-cd "${CUSTOM_DIR}"/plugins/zsh-interactive-cd >/dev/null 2>&1 || error "Unable to install zsh-interactive-cd. Try again."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting "${CUSTOM_DIR}"/plugins/zsh-syntax-highlighting >/dev/null 2>&1 || error "Unable to install zsh-syntax-highlighting. Try again."
 }
 
 function install() {
@@ -86,7 +73,7 @@ function install() {
 
     # Install fzf
     echo -e "${TXT_GREEN}>${TXT_DEFAULT} Installing fzf..."
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf >/dev/null 2>&1
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf >/dev/null 2>&1 || error "Unable to install fzf. Try again."
     ~/.fzf/install --all >/dev/null 2>&1 || error "Unable to install fzf. Try again."
 
     # Install nvm
@@ -94,6 +81,10 @@ function install() {
     [[ -d "$XDG_DATA_HOME"/nvm ]] || mkdir -p "$XDG_DATA_HOME"/nvm
     curl -s https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash >/dev/null 2>&1 || error "Unable to install nvm. Try again."
     bash -c "source $XDG_DATA_HOME/nvm/nvm.sh && nvm install --lts >/dev/null 2>&1" || error "Unable to install nvm. Try again."
+
+    # Install plugins
+    echo -e "${TXT_GREEN}>${TXT_DEFAULT} Installing oh-my-zsh and plugins..."
+    install_omzsh
 
     # Sync dotfiles to home directory
     echo -e "${TXT_GREEN}>${TXT_DEFAULT} Installing dotfiles..."
@@ -104,14 +95,13 @@ function install() {
         --exclude "LICENSE" \
         -avh --no-perms . ~ >/dev/null 2>&1 || error "Unable to sync files. Try again."
 
-    # Install plugins
-    echo -e "${TXT_GREEN}>${TXT_DEFAULT} Installing plugins..."
-    install_plugins
-
-    # Change shell to zsh
+    # Change default shell to zsh
     echo -e "${TXT_GREEN}>${TXT_DEFAULT} Changing shell to zsh..."
     [ ! "$CODESPACES" ] && echo -e "${TXT_GREEN}>${TXT_DEFAULT} Enter your password if prompted."
     sudo chsh "$(id -un)" --shell "/usr/bin/zsh" >/dev/null 2>&1 || error "Unable to change shell. Change it manually."
+
+    # Reload terminal
+    zsh -c "source ~/.zshrc" >/dev/null 2>&1 || error "Unable to reload terminal. Try again."
 
     echo -e "${TXT_GREEN}>${TXT_DEFAULT} Done. Reload your terminal."
 }
