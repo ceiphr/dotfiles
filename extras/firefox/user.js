7fc0b41a97617cd971e0fc20325017352b8ee4a4
Ari Birnbaum <ari@ceiphr.com>
2023-06-25 01:17:42 -0400

    Minor clean up of Firefox CSS.

--- extras/firefox/user.js
+++ extras/firefox/user.js
@@ -4,15 +4,15 @@ user_pref("apz.overscroll.enabled", true);
 // Improve performance
 user_pref("gfx.webrender.all", true);
 
+// Disable Pocket
+user_pref("extensions.pocket.enabled", false);
+
+// Enable userChrome.css/userContent.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
 // GNOME theme-specific settings
 user_pref("gnomeTheme.bookmarksToolbarUnderTabs", true);
 user_pref("gnomeTheme.hideSingleTab", true);
 user_pref("gnomeTheme.hideWebrtcIndicator", true);
 user_pref("gnomeTheme.normalWidthTabs", false);
 user_pref("gnomeTheme.spinner", true);
-
-// Disable Pocket
-user_pref("extensions.pocket.enabled", false);
-
-// Enable userChrome.css/userContent.css
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
